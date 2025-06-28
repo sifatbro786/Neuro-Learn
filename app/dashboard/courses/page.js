d4b32a336866af6ed0e211d9fbf414c0871ab94a
sifatbro786 <solaimanislamsifat@gmail.com>
2025-06-28 21:34:21 +0600

    bug fixed in Dashboard courses page

--- app/dashboard/courses/page.js
+++ app/dashboard/courses/page.js
@@ -2,6 +2,8 @@ import { COURSE_DATA, getInstructorDashboardData } from "@/lib/dashboard-helper"
 import { columns } from "./_components/columns";
 import { DataTable } from "./_components/data-table";
 
+export const dynamic = "force-dynamic";
+
 const CoursesPage = async () => {
     const courses = await getInstructorDashboardData(COURSE_DATA);
 
